a19ff900990e687549a2edcaad8344d9cee81823
Alexander Luz <alexluz@live.de>
2022-08-02 15:23:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,18 +56,15 @@ if __name__ == '__main__':
 ####################################################### SPEECH TO TEXT
  
 
-# get audio from the microphone
-r = sr.Recognizer()
+# read the audio file
+with sr.AudioFile('audio.wav') as source:
+    audio = r.record(source)
 
-# recognize speech using Google Speech Recognition
+# recognize the speech in the audio
 try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    st.session_state.mic_input = r.recognize_google(st.session_state.val) 
-    st.write(st.session_state.mic_input)
+    text = r.recognize_google(audio)
+    print(text)
 except sr.UnknownValueError:
-    st.write("Google Speech Recognition could not understand audio")
+    print("Could not understand audio")
 except sr.RequestError as e:
-    st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
-
+    print("Could not request results; {0}".format(e))
